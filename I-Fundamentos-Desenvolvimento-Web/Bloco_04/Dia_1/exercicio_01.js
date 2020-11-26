ad225e84ccdd0fdb85e2d45bd7c100fc004aca49
cleber-teixeira <cleberlt@gmail.com>
2020-11-26 01:22:57 -0300

    Exercicio 1 - Faça cinco programas, um para cada operação aritmética básica.

---
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_04/Dia_1/exercicio_01.js
@@ -0,0 +1,26 @@
+//Definir variáveis
+let a = 10;
+let b = 5;
+
+//-Adição
+let soma = a+b;
+
+//-Subtração
+let sub = a-b;
+
+//-Multiplicação
+let mult = a*b;
+
+//-Divisão
+let div = a/b;
+
+//-Módulo
+let mod = a%b;
+
+//-Resultados
+console.log('Sendo "a=' + a + '" e "b=' + b + '" os resultados calculados são:');
+console.log('A soma de "a+b" é igual a: ' + soma);
+console.log('A subtração de "a-b" é igual a: ' + sub);
+console.log('A multiplicação de "a*b" é igual a: ' + mult);
+console.log('A divisão de "a/b" é igual a: ' + div);
+console.log('O resto da divisão entre "a" e "b" é igual a: ' + mod);

43d92cce433f7e50e8dfac5abab04c5e91cb0244
cleber-teixeira <cleberlt@gmail.com>
2020-11-26 02:37:23 -0300

    Exercicio 2 - O programa retorna o maior de dois numeros.

---
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_04/Dia_1/exercicio_02.js
@@ -0,0 +1,14 @@
+//Declaração de variáveis
+let num1 = 45;
+let num2 = 54;
+let resposta = 0;
+
+//Compara o maior número
+if (num1 > num2) {
+    resposta = num1;
+} else {
+    resposta = num2;
+}
+
+//Resultado
+console.log('Sendo os valores "' + num1 + '" e "' + num2 + '", logo o valor "' + resposta + '" é o maior.');

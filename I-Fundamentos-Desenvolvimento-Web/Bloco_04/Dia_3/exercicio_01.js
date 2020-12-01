db0e9e6084d1ae5f27daaeb702e6bb06d8be4f63
cleber-teixeira <cleberlt@gmail.com>
2020-12-01 01:56:27 -0300

    Atualização - 01/12/2020 :white_check_mark:

---
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_04/Dia_3/exercicio_01.js
@@ -0,0 +1,28 @@
+/* 1- Para o primeiro exercício de hoje, faça um programa que,
+dado um valor n qualquer, seja n > 1 , imprima na tela um
+quadrado feito de asteriscos de lado de tamanho n.
+
+Por exemplo:
+
+n = 5
+
+*****
+*****
+*****
+*****
+***** */
+
+// Declara variável
+let value = 5;
+let square = '';
+
+// Imprime um quadrado de asterisco
+for (let index = 1; index <= value; index += 1) {
+    square += '*';
+}
+
+console.log('O valor informado foi: ' + value);
+
+for (let index = 1; index <= value; index += 1) {
+    console.log(square);
+}

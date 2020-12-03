9882e4fdd412096c338ee60416b6e072ea9dce14
cleber-teixeira <cleberlt@gmail.com>
2020-12-03 04:41:21 -0300

    03- Cria uma função que muda a cor do quadrado central. :white_check_mark:

--- I-Fundamentos-Desenvolvimento-Web/Bloco_05/Dia_1/exercicio_01.js
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_05/Dia_1/exercicio_01.js
@@ -4,9 +4,6 @@ Aqui você vai modificar os elementos já existentes utilizando apenas as funç�
     - document.getElementsByClassName()
     - document.getElementsByTagName()
 
-
-3. Crie uma função que mude a cor do quadrado vermelho para branco.
-4. Crie uma função que corrija o texto da tag <h1>.
 5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
@@ -19,13 +16,22 @@ function changeParagraphText () {
     paragraph.innerText = 'Daqui a dois anos me vejo bem sucedido com tudo que estou aprendendo na Trybe.';
 }
 changeParagraphText();
-
 /* 
 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 */
-
 function changeSquareColor() {
     let square = document.getElementsByClassName('main-content')[0];
     square.style.backgroundColor = 'rgb(76 , 164 , 109)';
 }
 changeSquareColor();
+/*
+3. Crie uma função que mude a cor do quadrado vermelho para branco.
+*/
+function changeSquareColor2() {
+    let square = document.getElementsByClassName('center-content')[0];
+    square.style.backgroundColor = 'rgb(255 , 255 , 255)';
+}
+changeSquareColor2();
+/*
+4. Crie uma função que corrija o texto da tag <h1>.
+*/

5bab5ea83bd7f40e86a5db8e8c09da8f408f7ef7
cleber-teixeira <cleberlt@gmail.com>
2020-12-04 06:38:17 -0300

    03- Adiciona tag <div> com class como filho em outra <div>. :white_check_mark:

--- I-Fundamentos-Desenvolvimento-Web/Bloco_05/Dia_2/exercicio_01.js
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_05/Dia_2/exercicio_01.js
@@ -2,7 +2,13 @@
 let titleH1 = document.createElement('h1');
 titleH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(titleH1);
+
 // 2. Adicione a tag div com a classe main-content como filho da tag body;
 let divMainContent = document.createElement('div');
 divMainContent.className = 'main-content';
 document.body.appendChild(divMainContent);
+
+// 3. Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
+let divCenterContent = document.createElement('div');
+divCenterContent.className = 'center-content';
+divMainContent.appendChild(divCenterContent);

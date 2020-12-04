3bf2156975ae7f947b7c43878845e2f928941a6c
cleber-teixeira <cleberlt@gmail.com>
2020-12-04 03:30:37 -0300

    01- Adiciona tag <h1> com texto no arquivo html. :white_check_mark:

---
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_05/Dia_2/exercicio_01.js
@@ -0,0 +1,5 @@
+// 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
+let titleH1 = document.createElement('h1');
+titleH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+document.body.appendChild(h1);
+// 2. Adicione a tag div com a classe main-content como filho da tag body;

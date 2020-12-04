dda00c831b6dacc14a3e2adfec1f33198fe6b797
cleber-teixeira <cleberlt@gmail.com>
2020-12-04 04:06:26 -0300

    02- Adiciona uma tag <div> com a classe main-content na <body>. :white_check_mark:

--- I-Fundamentos-Desenvolvimento-Web/Bloco_05/Dia_2/exercicio_01.js
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_05/Dia_2/exercicio_01.js
@@ -1,5 +1,8 @@
 // 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
 let titleH1 = document.createElement('h1');
 titleH1.innerText = 'Exercício 5.2 - JavaScript DOM';
-document.body.appendChild(h1);
+document.body.appendChild(titleH1);
 // 2. Adicione a tag div com a classe main-content como filho da tag body;
+let divMainContent = document.createElement('div');
+divMainContent.className = 'main-content';
+document.body.appendChild(divMainContent);

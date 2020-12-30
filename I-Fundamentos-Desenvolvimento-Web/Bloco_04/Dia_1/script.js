41525b10bd4bebea06775da3c15db3c3e0c95c05
cleber-teixeira <cleberlt@gmail.com>
2020-12-30 12:48:19 -0300

    Exercício 03 - O programa retorna o maior de três números. :white_check_mark:

--- I-Fundamentos-Desenvolvimento-Web/Bloco_04/Dia_1/script.js
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_04/Dia_1/script.js
@@ -30,10 +30,31 @@ function ex02() {
   const num1 = parseInt(document.getElementById('a2').value);
   const num2 = parseInt(document.getElementById('b2').value);
   if (num1 > num2) {
-      document.getElementById('resultado2').innerHTML = `Sendo os valores "${num1}" e "${num2}", logo o valor "${num1}" é o maior.`;
+      document.getElementById('resultado2').innerHTML = `Sendo os valores ${num1} e ${num2}, logo o valor ${num1} é o maior.`;
   } else if (num1 < num2) {
-    document.getElementById('resultado2').innerHTML = `Sendo os valores "${num1}" e "${num2}", logo o valor "${num2}" é o maior.`;
+    document.getElementById('resultado2').innerHTML = `Sendo os valores ${num1} e ${num2}, logo o valor ${num2} é o maior.`;
   } else {
     document.getElementById('resultado2').innerHTML = `Neste caso, não há maior valor`;
+  }
+}
+
+/*
+03- Faça um programa que retorne o maior de três números.
+Defina no começo do programa três variáveis com os 
+valores que serão comparados.
+*/
+
+function ex03() {
+  const num1 = parseInt(document.getElementById('a3').value);
+  const num2 = parseInt(document.getElementById('b3').value);
+  const num3 = parseInt(document.getElementById('c3').value);
+  if (num1 > num2 && num1 > num3) {
+      document.getElementById('resultado3').innerHTML = `Sendo os valores ${num1}, ${num2} e ${num3}, logo o valor ${num1} é o maior.`;
+  } else if (num2 > num1 && num2 > num3) {
+    document.getElementById('resultado3').innerHTML = `Sendo os valores ${num1}, ${num2} e ${num3}, logo o valor ${num2} é o maior.`;
+  } else if (num3 > num1 && num3 > num2) {
+    document.getElementById('resultado3').innerHTML = `Sendo os valores ${num1}, ${num2} e ${num3}, logo o valor ${num3} é o maior.`;
+  } else {
+    document.getElementById('resultado3').innerHTML = `Neste caso, não há maior valou, ou não foi identificado algum valor`;
   }
 }

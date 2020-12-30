3c7e27132d57b614e3c594e78d478718c2dcaa52
cleber-teixeira <cleberlt@gmail.com>
2020-12-30 11:49:07 -0300

    Exercício 02 - O programa retorna o maior de dois números. :white_check_mark:

--- I-Fundamentos-Desenvolvimento-Web/Bloco_04/Dia_1/script.js
+++ I-Fundamentos-Desenvolvimento-Web/Bloco_04/Dia_1/script.js
@@ -20,3 +20,20 @@ function ex01() {
   document.getElementById('modulo').innerHTML = `- O resto da divisão entre ${a} e ${b} é igual a ${modulo}`;
 }
 
+/*
+02- Faça um programa que retorne o maior de dois números.
+Defina no começo do programa duas variáveis com os 
+valores que serão comparados.
+*/
+
+function ex02() {
+  const num1 = parseInt(document.getElementById('a2').value);
+  const num2 = parseInt(document.getElementById('b2').value);
+  if (num1 > num2) {
+      document.getElementById('resultado2').innerHTML = `Sendo os valores "${num1}" e "${num2}", logo o valor "${num1}" é o maior.`;
+  } else if (num1 < num2) {
+    document.getElementById('resultado2').innerHTML = `Sendo os valores "${num1}" e "${num2}", logo o valor "${num2}" é o maior.`;
+  } else {
+    document.getElementById('resultado2').innerHTML = `Neste caso, não há maior valor`;
+  }
+}

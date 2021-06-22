b6d20b56fa17c75678997c7ac7ba3207a261edae
cleber-teixeira <cleberlt@gmail.com>
2021-06-22 13:46:53 -0300

    Criar um "jogo de adivinhação" em que a pessoa ganha se acertar qual foi o número aleatório gerado. :white_check_mark:

--- III-Desenvolvimento-Back-end/Bloco_26/Dia_1/velocidade.js
+++ III-Desenvolvimento-Back-end/Bloco_26/Dia_1/velocidade.js
@@ -1,3 +1,17 @@
+/* Exercício 6
+  6. Vamos criar mais um script. Dessa vez, para calcular a velocidade média de um carro numa corrida
+
+    1. A fórmula para calcular velocidade média é distância / tempo .
+
+    2. Armazene o script no arquivo velocidade.js.
+
+    3. Agora, permita que o script seja executado através do comando npm run velocidade . Para isso, crie a chave velocidade dentro do objeto scripts no package.json .
+
+    4. Utilize o readline-sync para solicitar os dados à pessoa.
+
+    5. Considere a distância em metros e o tempo em segundos. Repare que, agora, estamos trabalhando com números inteiros.
+*/
+
 // A fórmula para calcular velocidade média é distância / tempo.
 
 const { questionInt } = require('readline-sync');

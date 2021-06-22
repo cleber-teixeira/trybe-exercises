ff13df459d09f4e2742d5c0f4254df54175f979b
cleber-teixeira <cleberlt@gmail.com>
2021-06-22 12:54:59 -0300

    Refatoração - IMC com "input". :white_check_mark:

--- III-Desenvolvimento-Back-end/Bloco_26/Dia_1/imc.js
+++ III-Desenvolvimento-Back-end/Bloco_26/Dia_1/imc.js
@@ -1,11 +1,14 @@
 //A fórmula para calcular o IMC é peso / altura² .
+// const peso = 85;
+// const altura = 1.75;
 
-const peso = 85;
-const altura = 1.75;
+const { questionFloat } = require('readline-sync');
 
-function imc(p, a) {
-  const result = (p / Math.pow(a, 2)).toFixed(2);
+function imc() {
+  const peso = questionFloat('Qual o seu peso? (kg) ');
+  const altura = questionFloat('Qual a sua altura? (m) ');
+  const result = (peso / Math.pow(altura, 2)).toFixed(2);
   return console.log(`IMC: ${result}`);
 };
 
-imc(peso, altura);
+imc();

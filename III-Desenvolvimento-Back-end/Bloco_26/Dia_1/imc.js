f025ae1a7cd30625cef62c2d70ace14caac11d0c
cleber-teixeira <cleberlt@gmail.com>
2021-06-22 13:08:31 -0300

    Imprimir na tela em qual categoria o IMC da pessoa se enquadra. :white_check_mark:

--- III-Desenvolvimento-Back-end/Bloco_26/Dia_1/imc.js
+++ III-Desenvolvimento-Back-end/Bloco_26/Dia_1/imc.js
@@ -8,7 +8,22 @@ function imc() {
   const peso = questionFloat('Qual o seu peso? (kg) ');
   const altura = questionFloat('Qual a sua altura? (m) ');
   const result = (peso / Math.pow(altura, 2)).toFixed(2);
-  return console.log(`IMC: ${result}`);
+  if(result < 18.5) {
+    return console.log(`IMC: ${result}, Situação: Abaixo do peso (magreza)`);
+  }
+  if(result < 24.9) {
+    return console.log(`IMC: ${result}, Situação: Peso normal`);
+  }
+  if(result < 29.9) {
+    return console.log(`IMC: ${result}, Situação: Acima do peso (sobrepeso)`);
+  }
+  if(result < 34.9) {
+    return console.log(`IMC: ${result}, Situação: Obesidade grau I`);
+  }
+  if(result < 39.9) {
+    return console.log(`IMC: ${result}, Situação: Obesidade grau II`);
+  }
+  return console.log(`IMC: ${result}, Situação: Obesidade grau III e IV`);
 };
 
 imc();
